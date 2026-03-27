05fdc7232cd1ddeb9d4c68c09ad850fef3defce0
Eduardo-Rubel <ercanavarro@minha.fag.edu.br>
2026-03-27 08:41:37 -0300

    Atividade 5

---
+++ Desenvolvimento Web/Atividade05/main.js
@@ -0,0 +1,11 @@
+const valorConta = 500
+const percentualGorjeta = 15
+
+let Gorjeta
+let valorTotal
+
+Gorjeta = valorConta * (percentualGorjeta / 100 )
+
+valorTotal = valorConta + Gorjeta
+
+console.log("O Valor total é :",valorTotal,"\nO Total da Gorjeta é :", Gorjeta)
